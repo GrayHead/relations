ec890b3fd0419147b015d1de7cb43e91744aa7eb
GrayHead <ss.zhuravlov@gmail.com>
2021-03-18 21:55:45 +0200

    java + react/ng

--- src/main/java/ua/com/owu/relations/controllers/UserController.java
+++ src/main/java/ua/com/owu/relations/controllers/UserController.java
@@ -16,73 +16,90 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"})
 public class UserController {
 
 
     private UserDAO userDAO;
 
 
+    // /users
     @GetMapping()
     public List<User> getUsers() {
         return userDAO.findAll();
     }
 
-    @GetMapping("/save")
-    public void save() {
-        User user = new User();
-        user.setName("vasya");
-        List<Product> productList = new ArrayList<>();
-        productList.add(new Product("milk"));
-        productList.add(new Product("kokos"));
-        user.setProducts(productList);
-
-//        for (Product product : productList) {
-//            product.setUser(user);
-//        }
-        userDAO.save(user);
-    }
-
-
-    @GetMapping("/saveWithCars")
-    public void saveWithCars() {
-
-        User user = new User("max");
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("suba"));
-        cars.add(new Car("mazda"));
-        user.setCars(cars);
-        userDAO.save(user);
-
-    }
-
-
-    @PostMapping("/save")
-    public void saveUserWithImage(@RequestParam String name, @RequestParam MultipartFile image) throws IOException {
-
-
-        String pathToImageFolder = System.getProperty("user.home") + File.separator + "pictures2" + File.separator + image.getOriginalFilename();
-        image.transferTo(new File(pathToImageFolder));
-
-        User user = new User();
-        user.setName(name);
-
-        String filename = image.getOriginalFilename();
-        user.setImg(filename);
-        userDAO.save(user);
-
-//        System.getProperties().keySet().forEach(System.out::println);
-//        System.out.println(System.getProperty("user.home"));
-
-    }
 
     @GetMapping("/{id}")
-    public User getImageOfUser(@PathVariable int id) {
-        System.out.println(id);
-
+    public User getUser(@PathVariable int id) {
         User one = userDAO.getOne(id);
-        System.out.println(one);
         return one;
-
     }
+
+    @PostMapping("/save")
+    public void save(@RequestBody User user) {
+        userDAO.save(user);
+    }
+
+//    @DeleteMapping("/${id}")
+
+
+//    @GetMapping("/save")
+//    public void save() {
+//        User user = new User();
+//        user.setName("vasya");
+//        List<Product> productList = new ArrayList<>();
+//        productList.add(new Product("milk"));
+//        productList.add(new Product("kokos"));
+//        user.setProducts(productList);
+//
+////        for (Product product : productList) {
+////            product.setUser(user);
+////        }
+//        userDAO.save(user);
+//    }
+//
+//
+//    @GetMapping("/saveWithCars")
+//    public void saveWithCars() {
+//
+//        User user = new User("max");
+//        List<Car> cars = new ArrayList<>();
+//        cars.add(new Car("suba"));
+//        cars.add(new Car("mazda"));
+//        user.setCars(cars);
+//        userDAO.save(user);
+//
+//    }
+//
+//
+//    @PostMapping("/save")
+//    public void saveUserWithImage(@RequestParam String name, @RequestParam MultipartFile image) throws IOException {
+//
+//
+//        String pathToImageFolder = System.getProperty("user.home") + File.separator + "pictures2" + File.separator + image.getOriginalFilename();
+//        image.transferTo(new File(pathToImageFolder));
+//
+//        User user = new User();
+//        user.setName(name);
+//
+//        String filename = image.getOriginalFilename();
+//        user.setImg(filename);
+//        userDAO.save(user);
+//
+////        System.getProperties().keySet().forEach(System.out::println);
+////        System.out.println(System.getProperty("user.home"));
+//
+//    }
+//
+//    @GetMapping("/{id}")
+//    public User getImageOfUser(@PathVariable int id) {
+//        System.out.println(id);
+//
+//        User one = userDAO.getOne(id);
+//        System.out.println(one);
+//        return one;
+//
+//    }
 
 }
